844284dab9b99ecaad11cf951c6461feb028ff73
zakikurniawan27 <kurniawan.zak279@gmail.com>
2024-07-18 09:42:21 +0800

    feateure/penghuni: add penghuni page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Penghuni from "./pages/penghuni/Penghuni.jsx";
 
 const rout = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/penghuni",
+    element: <Penghuni />,
   },
 ]);
 
